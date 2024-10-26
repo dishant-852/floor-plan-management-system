bdc521f32481dff3c24560f39ee70514b6d6134c
dishant-852 <dishantgupta852@gmail.com>
2024-10-26 18:24:36 +0530

    adding dotenv

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,16 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
+console.log(process.env);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAe-6Nu2kRcLAZDXE-HlgwgmDHQS_L1roE",
-  authDomain: "floor-plan-management-system.firebaseapp.com",
-  projectId: "floor-plan-management-system",
-  databaseURL:"https://floor-plan-management-system-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  storageBucket: "floor-plan-management-system.appspot.com",
-  messagingSenderId: "30003136164",
-  appId: "1:30003136164:web:1a5d51d92aa00c3a378831",
-  measurementId: "G-74Z7DVEZND"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 
 const app = initializeApp(firebaseConfig);
